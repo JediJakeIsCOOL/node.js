abe821ad95dcad8786b13156508875972fe29c9b
Jacob Anderson <jedijake@Jacobs-MBP.localdomain>
2018-03-28 15:56:50 -0500

    chat app and stuff

---
+++ chatApp/index.js
@@ -0,0 +1,31 @@
+// var express = require('express')
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+app.get('/', function(req, res){
+    
+    res.sendFile(__dirname+'/index.html') 
+    console.log(__dirname)
+})
+// io.on('connection', function(socket){
+//     socket.on('chat message', function(msg){
+//       console.log('message: ' + msg);
+//     });
+//   });
+
+  io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+  });
+      
+// socket.on('disconnect', function(){
+//         console.log('user disconnected');
+// })
+
+// app.use(express.static('public'))
+
+http.listen(3000, function(){
+    console.log('listening on 3000')
+})
